fbb1e13003f36050954a8167c1414963038d6fe5
streamshqip <48894912+streamshqip@users.noreply.github.com>
2019-03-25 01:25:51 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,159 @@
+$(document).ready(function(){
+    $("#sport").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="sport.html" frameborder="0" allowfullscreen></iframe>');
+   });
+});
+
+
+
+$(document).ready(function(){
+    $("#a2cnn").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="https://www.youtube.com/embed/-OZRuRAqgpo?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#oranews").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="http://www.oranews.tv/tvplayer/oralive.html?v=HD" frameborder="0" webkitallowfullscreen="" mozkitallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#news24").click(function(){   
+       $('#channel').html('<iframe autoplay width="100%" class="theframe" src="https://balkanweb.com/news24/news24_live.php" frameborder="0" webkitallowfullscreen="" mozkitallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#klanhd").click(function(){   
+       $('#channel').html('<iframe src="http://canalesportivo.xyz/channel/clarpplayer/tvklan.php" width="100%" height="100%" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay;"></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#topchannel").click(function(){   
+       $('#channel').html('<iframe src="http://canalesportivo.xyz/channel/clarpplayer/topch.php" width="100%" height="100%" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay;"></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#oranews").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="http://www.oranews.tv/tvplayer/oralive.html?v=HD" frameborder="0" webkitallowfullscreen="" mozkitallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#vizionplus").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="https://www.vizionplus.tv/livestream2/examples/flowplayer/" frameborder="0" webkitallowfullscreen="" mozkitallowfullscreen="" allowfullscreen=""> </iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#faxnews").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="https://livestream.ekranet.com/faxnews&autoplay=yes" frameborder="0" webkitallowfullscreen="" mozkitallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#abcnews").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="https://www.youtube.com/embed/mNEQA17HsT8?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#channelone").click(function(){   
+       $('#channel').html('<iframe width="280" height="186" class="theframe" src="http://www.oranews.tv/tvplayer/channel1.html?v=HD" frameborder="0" webkitallowfullscreen="" mozkitallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#topnews").click(function(){   
+       $('#channel').html('<iframe frameborder="0" width="100%" class="theframe" src="https://www.dailymotion.com/embed/video/x6inuzo?autoPlay=1" allowfullscreen="" allow="autoplay"></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#zjarrtv").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="http://livestream.ekranet.com/zjarrtv&autoplay=yes" frameborder="0" webkitallowfullscreen="" mozkitallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#scantv").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="http://livestream.ekranet.com/scantv&amp;autoplay=yes" frameborder="0" webkitallowfullscreen="" mozkitallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#ufotv").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="" frameborder="0" allowfullscreen></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#rtshsport").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="https://livestream.ekranet.com/rtshsport1&autoplay=yes" frameborder="0" webkitallowfullscreen="" mozkitallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#rtsh1").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="https://livestream.ekranet.com/rtshhd1" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#rtsh2").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="https://livestream.ekranet.com/rtsh2hd1" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#rtsh3").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="https://livestream.ekranet.com/rtsh3hd1" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#rtshfilm").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="https://livestream.ekranet.com/rtshfilm_240p" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#rtshshqip").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="https://livestream.ekranet.com/rtshshqip1" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#rtshkuvend").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="https://livestream.ekranet.com/rtshkuvendweb" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#rtshmuzike").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="https://livestream.ekranet.com/rtshmsicweb" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#rtshfemije").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="https://livestream.ekranet.com/rtshfemije_240p" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#rtsh24").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="https://livestream.ekranet.com/rtsh24" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+$(document).ready(function(){
+    $("#rtshkorca").click(function(){   
+       $('#channel').html('<iframe width="100%" class="theframe" src="https://livestream.ekranet.com/tvkorca" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>');
+   });
+});
+
+
